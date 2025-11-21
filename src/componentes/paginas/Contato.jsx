0a46ba7a1eebe864ceabe724942a080ff7f5df84
d4v1dm7rs <ds093461@gmail.com>
2025-11-21 00:15:07 -0300

    Fazendo a seção contato

--- src/componentes/paginas/Contato.jsx
+++ src/componentes/paginas/Contato.jsx
@@ -1,7 +1,33 @@
+import styles from '../css/Contato.module.css'
+
 function Contato() {
     return(
-        <div>
+        <div className={styles.containerGeral}>
+            <div className={styles.container}>
+                <h1>Contate-nos</h1>
 
+                <div className={styles.content}>
+                    <h2>Precisa de ajuda?</h2>
+                    <p>Preencha o formulario abaixo</p>
+                    
+                    <form action="/">
+                        <div className={styles.info}>
+                            <input type="text" id='nome' placeholder='Primeiro nome'/>
+                            <input type="text" id='nome' placeholder='Ultimo nome'/>
+                        </div>
+                        <div className={styles.info}>
+                            <input type="text" id='email' placeholder='E-mail'/>
+                            <input type="tel" id='telefone' placeholder='Telefone'/>
+                        </div>
+                        <div className={styles.info}>
+                            <textarea name="Mensagem" id="mensagem" placeholder='Adicione aqui os detalhes do seu problema'></textarea>
+                        </div>
+                        <div className={styles.info}>
+                            <button type='submit'>Enviar</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
         </div>
     )
 }
