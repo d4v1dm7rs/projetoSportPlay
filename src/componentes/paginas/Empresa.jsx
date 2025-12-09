29a0b6145d436a47303d1dc4178ff9a5a0cd0533
d4v1dm7rs <ds093461@gmail.com>
2025-12-09 09:10:51 -0300

    Começando a fazer a pagina da empresa

--- src/componentes/paginas/Empresa.jsx
+++ src/componentes/paginas/Empresa.jsx
@@ -1,7 +1,19 @@
+import styles from '../css/Empresa.module.css'
+
 function Empresa() {
     return (
-        <div>
-            
+        <div className={styles.empresaContainer}>
+            <h1>Equipe</h1>
+            <div className={styles.empresaContent}>
+                <div className={styles.empresaBloco}>
+                    <img src="" alt="" />
+                    <p>David</p>
+                </div>
+                <div className={styles.empresaBloco}>
+                    <img src="" alt="" />
+                    <p>Maria Isabel</p>
+                </div>
+            </div>
         </div>
     )
 }
